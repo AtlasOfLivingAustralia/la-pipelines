f0531e4c4226b5041b9fb27d0d29ebd2b7576791
Dave Martin <djtfmartin@gmail.com>
2020-01-23 18:00:29 +0000

    more bash scripts for testing

---
+++ sample.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]
+  then
+    echo "Please supply a data resource UID"
+    exit 1
+fi
+
+java -Xmx8g -Xmx8g -XX:+UseG1GC -cp pipelines/target/pipelines-1.0-SNAPSHOT-shaded.jar au.org.ala.pipelines.beam.ALASamplingPipeline \
+ --datasetId=$1 \
+ --attempt=1 \
+ --runner=SparkRunner \
+ --inputPath=/data/pipelines-data \
+ --targetPath=/data/pipelines-data \
+ --metaFileName=indexing-metrics.txt \
+ --properties=pipelines.properties \
