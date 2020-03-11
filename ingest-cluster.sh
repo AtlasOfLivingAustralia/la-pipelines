6757d74b698938279de22e59f208300bee1e292e
Dave Martin <djtfmartin@gmail.com>
2020-03-11 12:35:36 +0000

    IncludeSampling and timings

--- ingest-cluster.sh
+++ ingest-cluster.sh
@@ -6,6 +6,7 @@ if [ $# -eq 0 ]
     exit 1
 fi
 
+SECONDS=0
 ./dwca-arvo.sh $1
 groovy interpret.groovy $1
 groovy export-latlng.groovy $1
@@ -13,4 +14,8 @@ groovy export-latlng.groovy $1
 ./sample-cache.sh $1
 groovy index.groovy $1
 
+duration=$SECONDS
+echo "Full ingest took $(($duration / 60)) minutes and $(($duration % 60)) seconds."
+
 curl -X GET "http://aws-quoll-1.ala:8983/solr/admin/collections?action=RELOAD&name=biocache"
+
