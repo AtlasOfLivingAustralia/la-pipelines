a12484aabf6205db0de172725b121f634d971ab6
Dave Martin <djtfmartin@users.noreply.github.com>
2020-05-14 22:19:12 +0100

    removed outdated reference

--- ingest-cluster.sh
+++ ingest-cluster.sh
@@ -11,11 +11,11 @@ SECONDS=0
 groovy interpret.groovy $1
 groovy export-latlng.groovy $1
 ./sample.sh $1
-./sample-cache.sh $1
+./sample-avro-cluster.sh $1
 groovy index.groovy $1
 
 duration=$SECONDS
 echo "Full ingest took $(($duration / 60)) minutes and $(($duration % 60)) seconds."
 
-curl -X GET "http://aws-quoll-1.ala:8983/solr/admin/collections?action=RELOAD&name=biocache"
+curl -X GET "http://aws-solrcloud-quoll-1.ala:8983/solr/admin/collections?action=RELOAD&name=biocache"
 
