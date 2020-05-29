ebf9acb63efaadcf7706cc3586d9185de3ae9d25
Mahmoud <sadeghim@gmail.com>
2020-05-29 14:39:54 +1000

    Won't fix the issue, just manual command
    It can be easily done on jenkins.

--- dwca-avro-all.sh
+++ dwca-avro-all.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 rootDir=/data/biocache-load
+# !!!!!! need to run this command on one vm then run the script !!!!
+# rm -rf $rootDir/dr*.locked
 echo "#### DWCA-AVRO #####"
 SECONDS=0
-lockDirs=()
+tempDirs=()
 for file in $rootDir/dr*; do
   if [[ -d $file ]] && [[ $file =~ ${rootDir}/dr[0-9]+$ ]]; then
     datasetID=$(basename $file)
     if mkdir "$file.lockdir"; then
-      lockDirs+=("$file.lockdir")
+      tempDirs+=("$file.lockdir")
       # you now have the exclusive lock
       echo "[DWCA-AVRO] Starting dwca avro conversion for $datasetID....."
       ./dwca-avro.sh $datasetID
       echo "[DWCA-AVRO] Finished dwca avro conversion for $datasetID."
+      mkdir "$file.processed"
     else
       echo "[DWCA-AVRO] Skipping dwca avro conversion for $datasetID....."
     fi
@@ -19,14 +22,3 @@ for file in $rootDir/dr*; do
 done
 duration=$SECONDS
 echo "#### DWCA-AVRO - DWCA load of all took $(($duration / 60)) minutes and $(($duration % 60)) seconds."
-totalDrs=`ls -1q $rootDir/dr*/dr*.zip | wc -l`
-processedDrs=`ls -1q $rootDir/dr*.lockdir | wc -l`
-
-echo "#### DWCA-AVRO - Wait till all the DRs get processed successfully."
-while [[ "$totalDrs" != "$processedDrs" ]]; do
-  sleep 1s
-done
-
-for lockDir in "${lockDirs[@]}"; do
-  rmdir $lockDir
-done
