f4e30b22a3692a86d2d4f014b0726be8ce10072b
Mahmoud <sadeghim@gmail.com>
2020-05-29 14:02:44 +1000

    Fix the waiting to remove the lockDirs for load

--- dwca-avro-all.sh
+++ dwca-avro-all.sh
@@ -19,6 +19,13 @@ for file in $rootDir/dr*; do
 done
 duration=$SECONDS
 echo "#### DWCA-AVRO - DWCA load of all took $(($duration / 60)) minutes and $(($duration % 60)) seconds."
+totalDrs=`ls -1q $rootDir/dr*/dr*.zip | wc -l`
+processedDrs=`ls -1q $rootDir/dr*.lockdir | wc -l`
+
+echo "#### DWCA-AVRO - Wait till all the DRs get processed successfully."
+while [[ "$totalDrs" != "$processedDrs" ]]; do
+  sleep 1s
+done
 
 for lockDir in "${lockDirs[@]}"; do
   rmdir $lockDir
